ea3c2e2dc5276b1e87c408c29e88dc0d6095b424
Guga <Guga@Gugas-MacBook-Air.local>
2019-05-01 22:42:57 -0500

    first commit

--- src/lists/ArrayLists.java
+++ src/lists/ArrayLists.java
@@ -6,42 +6,34 @@ import java.util.List;
 
 public class ArrayLists {
 	public static void main(String[] args) {
-		
-		List<Integer> salaries=new ArrayList<>();
+		// line...
+		List<Integer> salaries = new ArrayList<>();
 		salaries.add(5000);
 		salaries.add(6000);
-		
+
 		salaries.add(0, 3500);
-		System.out.println(salaries); 
-		
+		System.out.println(salaries);
+
 		System.out.println(salaries.get(2));
-		
+
 		salaries.remove(new Integer(3500));
 		System.out.println(salaries);
-		
-		
-		
-		Iterator<Integer> it=salaries.iterator();
-			while(it.hasNext()){
+
+		Iterator<Integer> it = salaries.iterator();
+		while (it.hasNext()) {
 			System.out.println(it.next());
-			}
-			
-			
-			salaries.add(7600);
-			salaries.add(7600);
-			
-			System.out.println(salaries);
-			
-			int pos=salaries.lastIndexOf(7600);
-			System.out.println(pos);
-			
-			System.out.println(salaries.get(3));
-			
-			
-			
-			
-			
+		}
+
+		salaries.add(7600);
+		salaries.add(7600);
+
+		System.out.println(salaries);
+
+		int pos = salaries.lastIndexOf(7600);
+		System.out.println(pos);
+
+		System.out.println(salaries.get(3));
+
 	}
-	
-	
+
 }
